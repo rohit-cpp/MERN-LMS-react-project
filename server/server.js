f6a48250514c0ae4a2c89b1b2df68fdce5374d3d
Rohit <rrgawanderohit@gmail.com>
2025-04-20 19:50:14 +0530

    register login logout api done

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config";
 import cookieParser from "cookie-parser";
 import connectDB from "./config/mongoDB.js";
+import authRouter from "./routes/authRoutes.js";
 
 const app = express();
 
@@ -19,7 +20,11 @@ cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ credentials: true }));
+
+// API Endpoints
 app.get("/", (req, res) => res.send("Api Working"));
+app.use("/api/auth", authRouter);
+
 app.listen(PORT, () => {
   console.log(`Server is now Running at PORT ${PORT} `);
 });
